f3dd6f528135c45bdb12c31e78c264bfcd9381e3
ZsoltTirkala <tirkala.zsolt@gmail.com>
2020-10-22 13:07:36 +0200

    create entity and repository

---
+++ src/main/java/com/example/jpaseries/entity/Episode.java
@@ -0,0 +1,21 @@
+package com.example.jpaseries.entity;
+
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class Episode {
+
+    @Id
+    @GeneratedValue
+    private long id;
+}
