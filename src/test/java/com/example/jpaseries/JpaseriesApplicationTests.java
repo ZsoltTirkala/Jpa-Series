327a736f8cec3d8b6a4b7782a13ae0e3ba96e63c
ZsoltTirkala <tirkala.zsolt@gmail.com>
2020-10-22 12:25:48 +0200

    init project, add gitignore

---
+++ src/test/java/com/example/jpaseries/JpaseriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaseries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaseriesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
