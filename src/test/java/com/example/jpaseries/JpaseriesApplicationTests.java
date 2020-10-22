ee0171d4c591ae876299c0ddf4d70f2dc958ba69
ZsoltTirkala <tirkala.zsolt@gmail.com>
2020-10-22 17:08:50 +0200

    create one test, fixed db connection

--- src/test/java/com/example/jpaseries/JpaseriesApplicationTests.java
+++ src/test/java/com/example/jpaseries/JpaseriesApplicationTests.java
@@ -1,20 +1,45 @@
 package com.example.jpaseries;
 
+import com.example.jpaseries.entity.Episode;
+import com.example.jpaseries.repository.EpisodeRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@ExtendWith({SpringExtension.class})
 @DataJpaTest
-@RunWith(SpringRunner.class)
 @ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
 class JpaseriesApplicationTests {
 
+
+    @Autowired
+    private TestEntityManager testEntityManager;
+
+    @Autowired
+    private EpisodeRepository episodeRepository;
+
+
+
     @Test
-    void contextLoads() {
+    void savedOnEpisodeElement() {
+        Episode episode = Episode.builder().build();
+        episodeRepository.save(episode);
+        List<Episode> episodeList = episodeRepository.findAll();
+        assertThat(episodeList).hasSize(1);
     }
+
 
 }
